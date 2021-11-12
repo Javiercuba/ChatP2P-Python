d8c47c8ef38e07b9b218ca768d2c91d2945bc595
Javier Ernesto lopez del real <javierernesto2000@gmail.com>
2021-11-12 17:29:19 -0300

    Funcionando corretamente

--- client.py
+++ client.py
@@ -2,8 +2,10 @@ import rpyc
 
 
 if __name__ == "__main__":
-    print("calling the server")
     c = rpyc.connect("localhost", 18830)
     c.root.echo("Hello Server")
     nickname = input("Insira seu nickname: ")
     c.root.set_nickname(str(nickname))
+    while True:
+        msg = input("#")
+        c.root.set_mensage(str(msg))
--- server.py
+++ server.py
@@ -1,29 +1,19 @@
 import rpyc
 from rpyc.utils.server import ThreadedServer
 
-
+chat = ""
 class MyService(rpyc.Service):
 
     def exposed_set_nickname(self, nickname):
-        print(nickname)
         self.nickname = nickname
         
-
     def exposed_echo(self, message):
         print(message)
 
-
-    def exposed_set_mensage(self, msg, nickname):
-        global collection
-        item_msg = {
-                "dest": nickname,
-                        "msg": self.nickname + ": " + msg
-            }
-        collection.insert_one(item_msg)
-        # chat = self.nickname + ": " + msg
-    def exposed_add(self, x, y):
-        print('Result is: {}'.format(x + y))
-
+    def exposed_set_mensage(self, msg):
+        global chat       
+        chat = self.nickname + ": " + msg
+        print(chat)
 
 if __name__ == "__main__":
     server = ThreadedServer(MyService, port=18830)
